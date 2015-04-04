726b899c22c4885405081abaabb578beeda848ba
tomomura <am.n04a@gmail.com>
2015-04-04 17:56:09 +0900

    Dashboards日付と曜日の表示

---
+++ Classes/Controllers/WorkTimes/FTWorkTimesViewController.swift
@@ -0,0 +1,37 @@
+//
+//  FTWorkTimesViewController.swift
+//  TimeSheet
+//
+//  Created by TomomuraRyota on 2015/04/04.
+//  Copyright (c) 2015年 TomomuraRyota. All rights reserved.
+//
+
+import UIKit
+
+class FTWorkTimesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 31;
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as FTWorkTimesTableViewDayCell;
+        
+        cell.dayLabel.text = "2日";
+        cell.wdayLabel.text = "水";
+        
+        return cell;
+    }
+    
+}
---
+++ Classes/Views/WorkTimes/FTWorkTimesTableViewDayCell.swift
@@ -0,0 +1,27 @@
+//
+//  FTWorkTimesTableViewDayCell.swift
+//  TimeSheet
+//
+//  Created by TomomuraRyota on 2015/04/04.
+//  Copyright (c) 2015年 TomomuraRyota. All rights reserved.
+//
+
+import UIKit
+
+class FTWorkTimesTableViewDayCell: UITableViewCell {
+
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var wdayLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
